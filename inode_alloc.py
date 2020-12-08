45d721d3ba1cece7e8e23f74617a3ab51f7e5a10
Kody Quintana <Quintana.Kody@gmail.com>
2020-12-07 20:54:18 -0700

    Add more formatting

--- inode_alloc.py
+++ inode_alloc.py
@@ -3,7 +3,7 @@
 
 FILE_BLOCKS = 2000
 ENTRY_SIZE_BYTES = 4
-ENTRIES_PER_BLOCK = 15
+SLOTS_PER_TABLE = 15
 
 DIRECT_IN_INODE = 12
 
@@ -20,14 +20,16 @@ def find_index_blocks():
 
         if level == "within inode":
             num_index_blocks += 1  # this is the inode itself
-            remaining -= DIRECT_IN_INODE  # the direct block pointers
+
+            remaining -= DIRECT_IN_INODE  # the direct block pointers in the inode
 
             # Advance to next level
             level = "single indirect"
 
         elif level == "single indirect":
-            num_index_blocks += 1
-            remaining -= ENTRIES_PER_BLOCK
+
+            # Create a direct pointer table and fill its entries with direct pointers to file blocks
+            num_index_blocks += 1; remaining -= SLOTS_PER_TABLE
 
             # Advance to next level
             level = "double indirect"
@@ -38,12 +40,12 @@ def find_index_blocks():
             num_index_blocks += 1
 
             # While there are remaining "single indirect slots" in the "double indirect table"
-            indirect_slots = ENTRIES_PER_BLOCK
+            indirect_slots = SLOTS_PER_TABLE
             while indirect_slots > 0:
                 indirect_slots -= 1
 
                 # Create a direct pointer table and fill its entries with direct pointers to file blocks
-                num_index_blocks += 1; remaining -= ENTRIES_PER_BLOCK
+                num_index_blocks += 1; remaining -= SLOTS_PER_TABLE
 
                 # Always check if most recently added direct pointer table was enough to satisfy all file blocks
                 if remaining <= 0:
@@ -58,7 +60,7 @@ def find_index_blocks():
             num_index_blocks += 1
 
             # While there are remaining "double indirect slots" in the "triple indirect table"
-            double_indirect_slots = ENTRIES_PER_BLOCK
+            double_indirect_slots = SLOTS_PER_TABLE
             while double_indirect_slots > 0:
                 double_indirect_slots -= 1
 
@@ -66,12 +68,12 @@ def find_index_blocks():
                 num_index_blocks += 1
 
                 # While there are remaining "single indirect slots" in this particular "double indirect table"
-                indirect_slots = ENTRIES_PER_BLOCK
+                indirect_slots = SLOTS_PER_TABLE
                 while indirect_slots > 0:
                     indirect_slots -= 1
 
                     # Create a direct pointer table and fill its entries with direct pointers to file blocks
-                    num_index_blocks += 1; remaining -= ENTRIES_PER_BLOCK
+                    num_index_blocks += 1; remaining -= SLOTS_PER_TABLE
 
                     # Always check if most recently added direct pointer table was enough to satisfy all file blocks
                     if remaining <= 0:
@@ -82,7 +84,7 @@ def find_index_blocks():
 
 def find_total_bytes():
     """This will have to be something different if not all tables are 15"""
-    return find_index_blocks() * ENTRY_SIZE_BYTES * ENTRIES_PER_BLOCK
+    return find_index_blocks() * ENTRY_SIZE_BYTES * SLOTS_PER_TABLE
 
 
 def num_searches():
@@ -93,15 +95,15 @@ def num_searches():
         return 1
 
     # Single indirect:
-    if DIRECT_IN_INODE < FILE_BLOCKS <= (DIRECT_IN_INODE + ENTRIES_PER_BLOCK):
+    if DIRECT_IN_INODE < FILE_BLOCKS <= (DIRECT_IN_INODE + SLOTS_PER_TABLE):
         return 3
 
     # Double indirect:
-    if (DIRECT_IN_INODE + ENTRIES_PER_BLOCK) < FILE_BLOCKS <= ((DIRECT_IN_INODE + ENTRIES_PER_BLOCK) + ENTRIES_PER_BLOCK ** 2):
+    if (DIRECT_IN_INODE + SLOTS_PER_TABLE) < FILE_BLOCKS <= ((DIRECT_IN_INODE + SLOTS_PER_TABLE) + SLOTS_PER_TABLE ** 2):
         return 5
 
     # Triple indirect:
-    if ((DIRECT_IN_INODE + ENTRIES_PER_BLOCK) + ENTRIES_PER_BLOCK ** 2) < FILE_BLOCKS <= float("inf"):
+    if ((DIRECT_IN_INODE + SLOTS_PER_TABLE) + SLOTS_PER_TABLE ** 2) < FILE_BLOCKS <= float("inf"):
         return 7
 
     return "Should never get this string"
